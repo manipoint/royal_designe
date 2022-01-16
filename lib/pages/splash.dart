1980139d8441cdb5f505fc90558015350c5613d6
manipoint <manipoint@gmail.com>
2022-01-16 12:36:43 +0500

    first

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -9,13 +9,13 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Container(
-        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+       // margin: const EdgeInsets.only(left: 40.0, right: 40.0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Image.asset("assets/logo.png"),
+              Image.asset("assets/logo.png",height: 600,width: 500,),
                const SizedBox(width: 40.0),
               
             ],
